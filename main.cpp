ebdc1df1eb12cc3f18c2c73c3f7f48d66b664677
joshinils <joshua.laskowski@web.de>
2020-11-23 00:22:51 +0100

    ahaa!

--- main.cpp
+++ main.cpp
@@ -4,16 +4,46 @@ int main()
 {
     svg s;
     int V;
+    LineSpec L;
 
     // clang-format off
+    
     s.plot(V);std::cout << '\n';
+    s.plot(V, L);std::cout << '\n';
+
+    // s.plot(V, V, V);std::cout << '\n';
+    // s.plot(V, V, V, V, V);std::cout << '\n';
+    // s.plot(V, V, V, V, V, V, V);std::cout << '\n';
+
+    std::cout << "\n\n\n";
+
     s.plot(V, V);std::cout << '\n';
-    s.plot(V, V, V);std::cout << '\n';
-    s.plot(V, V, V, V);std::cout << '\n';
-    s.plot(V, V, V, V, V);std::cout << '\n';
-    s.plot(V, V, V, V, V, V);std::cout << '\n';
-    s.plot(V, V, V, V, V, V, V);std::cout << '\n';
-    s.plot(V, V, V, V, V, V, V, V);std::cout << '\n';
+    s.plot(V, V, L);std::cout << '\n';
+
+    std::cout << "\n\n\n";
+
+    s.plot(V, L, V, L, V, L);std::cout << '\n';
+    s.plot(V, L, V, L, V, L, V, L, V, L);std::cout << '\n';
+    s.plot(V, L, V, L, V, L, V, L, V, L, V, L);std::cout << '\n';
+
+    std::cout << "\n\n\n";
+
+    s.plot(V, V,    V, V   );std::cout << '\n';
+    s.plot(V, V,    V, V, L);std::cout << '\n';
+    s.plot(V, V, L, V, V   );std::cout << '\n';
+    s.plot(V, V, L, V, V, L);std::cout << '\n';
+
+    std::cout << "\n\n\n";
+
+    s.plot(V, V,    V, V,    V, V   );std::cout << '\n';
+    s.plot(V, V,    V, V,    V, V, L);std::cout << '\n';
+    s.plot(V, V,    V, V, L, V, V   );std::cout << '\n';
+    s.plot(V, V,    V, V, L, V, V, L);std::cout << '\n';
+    s.plot(V, V, L, V, V,    V, V   );std::cout << '\n';
+    s.plot(V, V, L, V, V,    V, V, L);std::cout << '\n';
+    s.plot(V, V, L, V, V, L, V, V   );std::cout << '\n';
+    s.plot(V, V, L, V, V, L, V, V, L);std::cout << '\n';
+
     // clang-format on
 
     return 0;
